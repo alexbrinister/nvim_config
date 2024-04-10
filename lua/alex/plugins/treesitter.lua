ab3d8bd983bfe14697442d02c665ac2ad96fd37e
Alex Necakov <alexnecakov@gmail.com>
2024-04-10 17:01:46 -0400

    added languages to treesitter

--- lua/alex/plugins/treesitter.lua
+++ lua/alex/plugins/treesitter.lua
@@ -1,7 +1,36 @@
-local M = {     
+return {     
 	"nvim-treesitter/nvim-treesitter",     
-	build = function()         
-		require("nvim-treesitter.install").update({ with_sync = true })()     
-	end, 
-	  }  
-return { M }
+	build = ":TSUpdate",         
+	config = function()
+		require'nvim-treesitter.configs'.setup ({  
+			ensure_installed = { 
+				"c", 
+				"cpp",
+				"c_sharp", 
+				"cmake", 	
+				"css",
+				"csv", 
+				"html",
+				"javascript",
+				"json",
+				"lua", 
+				"make",
+				"markdown",
+				"typescript",
+				"vim", 
+				"vimdoc", 
+				"xml",
+				"yaml",
+				"query" 
+			},    
+			sync_install = false,    
+			auto_install = true,
+			indent = {
+				enable = true
+			},
+			highlight = {     
+				enable = true,      
+			},
+		})
+	end
+}	  
