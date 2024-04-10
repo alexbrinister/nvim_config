46a489a506208340b774ea6a83520638df23052b
Alex Necakov <alexnecakov@gmail.com>
2024-04-10 16:05:32 -0400

    added telescope and treesitter

--- lua/alex/lazy_init.lua
+++ lua/alex/lazy_init.lua
@@ -10,4 +10,7 @@ if not vim.loop.fs_stat(lazypath) then
 	}) 
 end 
 vim.opt.rtp:prepend(lazypath)  
-require("lazy").setup(plugins, opts)     
+require("lazy").setup({
+	spec = "alex.plugins",
+	change_detection = {notify = false }
+})
