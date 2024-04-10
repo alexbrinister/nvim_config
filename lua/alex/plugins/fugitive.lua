24e28a1b04d01b49ab04398482edaab790c1c23e
Alex Necakov <alexnecakov@gmail.com>
2024-04-10 17:54:36 -0400

    adding push and pull remaps

--- lua/alex/plugins/fugitive.lua
+++ lua/alex/plugins/fugitive.lua
@@ -2,5 +2,7 @@ return{
 	"tpope/vim-fugitive",
 	config = function()
 		vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+		vim.keymap.set("n", "<leader>gp", vim.cmd.Git('push'))
+		vim.keymap.set("n", "<leader>gP", vim.cmd.Git('pull'))
 	end
 }
