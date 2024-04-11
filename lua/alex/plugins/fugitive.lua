1971ab67456d94cf4ddee64b713f3a2e2da72353
Alex Necakov <alexnecakov@gmail.com>
2024-04-10 21:14:04 -0400

    removed broken git push and pull sets

--- lua/alex/plugins/fugitive.lua
+++ lua/alex/plugins/fugitive.lua
@@ -2,7 +2,5 @@ return{
 	"tpope/vim-fugitive",
 	config = function()
 		vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
-		vim.keymap.set("n", "<leader>gp", vim.cmd.Git('push'))
-		vim.keymap.set("n", "<leader>gP", vim.cmd.Git('pull'))
 	end
 }
