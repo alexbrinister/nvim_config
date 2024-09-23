d6e45e0b834498c9243f98f6c529088253315bb4
Alex Necakov <alexnecakov@gmail.com>
2024-09-23 08:17:03 -0400

    fixed powershell

--- lua/alex/set.lua
+++ lua/alex/set.lua
@@ -26,13 +26,14 @@ vim.opt.isfname:append("@-@")
 
 vim.opt.colorcolumn = "80"
 
-if (vim.loop.os_uname().sysname == "Windows_NT")
-then
-    vim.opt.shell = vim.fn.executable "pwsh" and "pwsh" or "powershell"
-    vim.opt.shellcmdflag =
-    "-NoLogo -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.UTF8Encoding]::new();$PSDefaultParameterValues[''Out-File:Encoding'']=''utf8'';Remove-Alias -Force -ErrorAction SilentlyContinue tee;"
-    vim.opt.shellredir = "2>&1 | %%{ '$_' } | Out-File %s; exit $LastExitCode"
-    vim.opt.shellpipe = "2>&1 | %%{ '$_' } | tee %s; exit $LastExitCode"
-    vim.opt.shellquote = ""
-    vim.opt.shellxquote = ""
+
+if vim.fn.has('win32') == 1 then
+    vim.o.shell = 'powershell'
 end
+
+vim.o.shellcmdflag =
+'-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;'
+vim.o.shellredir = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
+vim.o.shellpipe = '2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode'
+vim.o.shellquote = ''
+vim.o.shellxquote = ''
