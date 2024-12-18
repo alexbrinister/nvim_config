5c0103eb9b709704226811fe790ee79473983ec0
Alex Brinister <IrVQ55Gw9TZ7BW8e@tuta.io>
2024-12-17 22:38:27 -0500

    added starter (non-working) config for go debugger
    Signed-off-by: Alex Brinister <IrVQ55Gw9TZ7BW8e@tuta.io>

--- lua/alex/plugins/dap.lua
+++ lua/alex/plugins/dap.lua
@@ -2,38 +2,79 @@ return {
     "leoluz/nvim-dap-go",
     dependencies = {
         "mfussenegger/nvim-dap",
+        "rcarriga/nvim-dap-ui",
+        "nvim-neotest/nvim-nio"
     },
     config = function()
         local dap = require('dap')
 
-        dap.adapters.go = function(callback, config)
-            local handle
-            handle = function(req, resp)
-                if resp.event == 'terminated' then
-                    handle = nil
-                end
-                callback(req, resp)
+        dap.adapters.delve = function(callback, config)
+            if config.mode == 'remote' and config.request == 'attach' then
+                callback({
+                    type = 'server',
+                    host = config.host or '127.0.0.1',
+                    port = config.port or '38697'
+                })
+            else
+                callback({
+                    type = 'server',
+                    port = '${port}',
+                    executable = {
+                        command = 'dlv',
+                        args = { 'dap', '-l', '127.0.0.1:${port}', '--log', '--log-output=dap' },
+                        detached = vim.fn.has("win32") == 0,
+                    }
+                })
             end
-
-            -- TODO: detect ProjectRoot to get the port from the launched dap server
-            local port = 38667 -- check the port number in the delve logs
-            local host = '127.0.0.1'
-            local addr = string.format('%s:%s', host, port)
-            local opts = {
-                command = { 'curl', '-s', string.format('http://%s/json/list', addr) },
-                callback = handle
-            }
-
-            require('dap.process').run(opts)
         end
 
+
+        -- https://github.com/go-delve/delve/blob/master/Documentation/usage/dlv_dap.md
         dap.configurations.go = {
             {
-                type = 'go',
-                name = 'Debug',
-                request = 'launch',
-                program = '${file}',
+                type = "delve",
+                name = "Debug",
+                request = "launch",
+                program = "${file}"
             },
+            {
+                type = "delve",
+                name = "Debug test", -- configuration for debugging test files
+                request = "launch",
+                mode = "test",
+                program = "${file}"
+            },
+            -- works with go.mod packages and sub packages
+            {
+                type = "delve",
+                name = "Debug test (go.mod)",
+                request = "launch",
+                mode = "test",
+                program = "./${relativeFileDirname}"
+            }
         }
+
+        require("dapui").setup()
+
+        dap.listeners.after.event_initialized["dapui_config"] = function()
+            dapui.opn()
+        end
+
+        dap.listeners.before.event_terminated["dapui_config"] = function()
+            dapui.close()
+        end
+
+        dap.listeners.before.event_exited["dapui_config"] = function()
+            dapui.close()
+        end
+
+        -- Keymaps for debugging
+        vim.fn.sign_define('DapBreakpoint', { text = '🟥', texthl = '', linehl = '', numhl = '' })
+        vim.fn.sign_define('DapStopped', { text = '▶️', texthl = '', linehl = '', numhl = '' })
+        vim.keymap.set('n', '<F5>', require 'dap'.continue)
+        vim.keymap.set('n', '<F10>', require 'dap'.step_over)
+        vim.keymap.set('n', '<F11>', require 'dap'.step_into)
+        vim.keymap.set('n', '<F12>', require 'dap'.step_out)
+        vim.keymap.set('n', '<leader>b', require 'dap'.toggle_breakpoint)
     end
 }
